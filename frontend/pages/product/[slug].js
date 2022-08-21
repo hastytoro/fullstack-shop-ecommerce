8ba36c5d8385de47fa80748b85f2c1fb341fbdf0
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-21 07:06:32 +0200

    9) Styling our "detailed" individual product

--- frontend/pages/product/[slug].js
+++ frontend/pages/product/[slug].js
@@ -3,6 +3,14 @@ import { useRouter } from "next/router";
 import { useQuery } from "urql";
 import { GET_PRODUCT_QUERY } from "../../lib/query";
 
+import {
+  DetailWrapper,
+  DetailInfo,
+  Quantity,
+  Buy,
+} from "../../styles/SlugDetail";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+
 export default function SlugDetail() {
   // Fetch slug from the url with useRouter hook.
   const { query } = useRouter();
@@ -14,22 +22,28 @@ export default function SlugDetail() {
   const { data, fetching, error } = results;
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no! {error.message}</p>;
-
+  // Extract our data from the graphql backend query:
   const { title, description, price, image } = data.products.data[0].attributes;
   const imgUrl = image.data.attributes.formats.medium.url;
+
   return (
-    <div>
-      <img src={imgUrl} alt="" />
-      <div>
+    <DetailWrapper>
+      <img src={imgUrl} alt={title} />
+      <DetailInfo>
         <h3>{title}</h3>
         <p>{description}</p>
-      </div>
-      <div>
-        <span>{price}</span>
-        <button>+</button>
-        <p>0</p>
-        <button>-</button>
-      </div>
-    </div>
+        <Quantity>
+          <span>Quantity</span>
+          <button>
+            <AiFillMinusCircle />
+          </button>
+          <p>0</p>
+          <button>
+            <AiFillPlusCircle />
+          </button>
+        </Quantity>
+        <Buy>Add to cart</Buy>
+      </DetailInfo>
+    </DetailWrapper>
   );
 }
