85a216e85c93e3fece2519df1787cf12158a429a
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-24 05:09:51 +0200

    23) Success page and using getServerSideProps
    By exporting a function getServerSideProps we perform (Server-Side Rendering) from a page. Next.js will pre-render the page on each request, using the data returned by getServerSideProps.

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -36,8 +36,10 @@ export default async function expressService(req, res) {
             quantity: item.quantity,
           };
         }),
-        // Will bring visitors to either a success or failed page:
-        success_url: `${req.headers.origin}/success`,
+        // Will bring visitors to either a success or failed page.
+        // You need the following string to submit to the success page:
+        // `?&session_id={CHECKOUT_SESSION_ID}`
+        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/canceled`,
       });
       res.status(200).json(session);
